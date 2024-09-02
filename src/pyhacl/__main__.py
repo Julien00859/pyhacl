b6c0380419bc82fd42d0ad9fd176504c4d47274a
Julien Castiaux <julien.castiaux@gmail.com>
2024-09-02 23:00:26 +0200

    chacha-poly1305

--- src/pyhacl/__main__.py
+++ src/pyhacl/__main__.py
@@ -1,23 +1,27 @@
 import os
 import sys
 
-from .key_exchange import curve25519_51, curve25519_64
+from .aead import chacha_poly1305
+from .hashlib import sha256
 
 def main():
     if len(sys.argv) != 2:
         return f"usage: {__package__} <data>"
+    data = sys.argv[1].encode()
+    sha = sha256.oneshot(data)
 
-    alice_sk = os.urandom(32)
-    alice_pk = curve25519_51.secret_to_public(alice_sk)
+    key = os.urandom(32)
+    nonce = (0).to_bytes(12, 'big')
 
-    bob_sk = os.urandom(32)
-    bob_pk = curve25519_64.secret_to_public(bob_sk)
-
-    shared1 = curve25519_51.ecdh(alice_sk, bob_pk)
-    shared2 = curve25519_64.ecdh(bob_sk, alice_pk)
-
-    print(shared1)
-    print(shared2)
+    cipher, tag = chacha_poly1305.encrypt(
+        data, sha, key, nonce
+    )
+    print(len(cipher), cipher)
+    print(len(tag), tag)
+    text = chacha_poly1305.decrypt(
+        cipher, sha, key, nonce, tag
+    )
+    print(text.decode())
 
 
 sys.exit(main())
